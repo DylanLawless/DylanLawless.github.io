b1f9849438053f02c5e1d3f71876583c6e994fcf
lawless <dylan.lawless@epfl.ch>
2021-05-20 14:53:11 +0200

    Jekyll rails gem fixes for OS
    Former-commit-id: c642fecb2c97c92ae48eb1a4b22763b1fd559511

--- jek.sh
+++ jek.sh
@@ -11,17 +11,16 @@ open -a Safari http://127.0.0.1:4000
 # ps aux | grep jekyll
 # kill -9 "PID"
 
+# Errors due to OS updates.
 
-
-# with error: can't find gem jekyll (>= 0.a)
-# sudo rm -rf _site
+# e.g.: can't find gem jekyll (>= 0.a). Depending on machine:
+# If rails not installed,
+# sudo gem install rails
 # bundle update
-# bundle exec jekyll serve
-# 
-# And then always using 
-# bundle exec jekyll serve 
-# instead of 
-# sudo jekyll serve or 
-# jekyll serve.
 
+# sudo gem install jekyll bundler
+# sudo rm -rf _site
 
+# sudo chmod -R 777 /usr/local/lib/ruby/gems/
+# gem install bundler jekyll
+# bundle add webrick
