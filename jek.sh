a41bd0f0ab207bc8e78a4047c2e7236df1c45991
DylanLawless <dylan.lawless@epfl.ch>
2021-12-08 19:19:55 +0100

    brace for this update that may break cross-platform ruby/gems/jekyll
    Former-commit-id: 6b999d7731f8e6df16c747d93265c989d277cb22

--- jek.sh
+++ jek.sh
@@ -33,3 +33,13 @@ open -a Safari http://127.0.0.1:4000
 # sudo chmod -R 777 /usr/local/lib/ruby/gems/
 # gem install bundler jekyll
 # bundle add webrick
+
+
+# for ruby versions
+# installed with homebrew https://github.com/rbenv/rbenv
+# then after running check script,
+# installed:  rbenv install 2.7.0
+# set version: rbenv global 2.7.0
+# then: bundler install
+# gem install --user-install bundler jekyll
+# it is possible that this last update does the trick but I am not sure, and need to test out the rbenv versions to be sure. 
