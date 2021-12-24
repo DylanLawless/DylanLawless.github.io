d8b438abefd6f1df9b4682361c1a3043907b8bd0
DylanLawless <dylan.lawless@epfl.ch>
2021-12-24 17:34:17 +0100

    large file all genes
    Former-commit-id: 4dd7416a44f47e51b41ed6faaaf47038ea5d903e [formerly 1351c8ef3a9a44a63bb23fe0de7e7eb4a78e440a]
    Former-commit-id: 58fdf71a4ceaa5d633ad447aadeca9bb6b7152c4

---
+++ _site/topic/evidence_finder.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp ~/web/tools/genomic_tools/gene_evidence/output/clingen-gene_disease_validity_table_original.html ./
+cp ~/web/tools/genomic_tools/gene_evidence/output/clingen-gene_disease_validity_table_all_genes.html ./
+
+cp ~/web/tools/genomic_tools/itan_gof_lof/output/itan_gof_lof.html ./
