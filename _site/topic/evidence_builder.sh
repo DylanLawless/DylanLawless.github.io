240c09ebf7a2e6154ac7e67946b16c8afddd9c5d
DylanLawless <dylan.lawless@epfl.ch>
2021-12-27 13:48:10 +0100

    large file
    Former-commit-id: 145d53c26a54f9137f0ef2ae73598b617fa4baf2 [formerly ca7ec7a6713f865a85042f10ff80cf90a2cee326]
    Former-commit-id: f1e0db336f8bb64ba539e848c80c8aab91c21dfd

--- _site/topic/evidence_builder.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp ~/web/tools/genomic_tools/gene_illustrate/output/gene_illustrate.html ./
-cp ~/web/tools/genomic_tools/gene_illustrate/output/gnomad_table.html ./
--- _site/topic/phenotypic_cov.md
+++
@@ -1,1 +0,0 @@
-https://www.nature.com/articles/s41467-021-21283-4
